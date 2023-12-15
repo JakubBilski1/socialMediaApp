5416d72b723a31925504acc50da7c15d7483a8b3
JakubBilski1 <jakub.bilski@spoko.pl>
2023-12-15 15:41:32 +0100

    header footer init

---
+++ client/src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="flex flex-row justify-center items-center text-white p-4 h-1/4 border-r border-gray-600">
+            <p>©{new Date().getFullYear()} Social Frame | All Rights Reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
