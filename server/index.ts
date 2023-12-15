158c72653e93da9b8c4a2ba1e50e256322ff1e2c
JakubBilski1 <jakub.bilski@spoko.pl>
2023-12-15 22:19:11 +0100

    home endpoint

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,15 @@ import express, { Express } from 'express';
 import authRoute from './routes/authRoute';
 
 const app: Express = express();
-const port: number = 5000;
+const port: number = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRoute);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
