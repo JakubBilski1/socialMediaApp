b13f687baee5d9ce9536ae652ac2f40ec8c28b9c
JakubBilski1 <jakub.bilski@spoko.pl>
2023-12-21 15:40:08 +0100

    index updates

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,29 @@
 import express, { Express } from 'express';
 import authRoute from './routes/authRoute';
+import userRoute from './routes/userRoute';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app: Express = express();
 const port: number = 8000;
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionsSuccessStatus: 200,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
+
+app.use(cors(corsOptions));
+
 app.use("/auth", authRoute);
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/u", userRoute);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
